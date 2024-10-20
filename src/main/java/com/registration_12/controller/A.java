1b77e4679770e411e2bc03afc253c228088a84cd
Ihwardas <ishwar39@gmail.com>
2024-10-20 21:15:26 +0530

    wwwe

--- src/main/java/com/registration_12/controller/A.java
+++ src/main/java/com/registration_12/controller/A.java
@@ -2,6 +2,6 @@ package com.registration_12.controller;
 
 public class A {
     public static void main(String[] args) {
-        System.out.println("hello brother");
+        System.out.println("hello brother kal jarur anna");
     }
 }
